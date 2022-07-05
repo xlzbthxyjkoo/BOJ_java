b73cb1810810b26fe157b06383315e121b3a2ff7
Yeji Koo <82144134+xlzbthxyjkoo@users.noreply.github.com>
2022-07-05 20:14:53 +0900

    Create 10172.java

---
+++ 10172.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+
+    char quotes = '\u0022'; //쌍따옴표 유니코드
+    
+    System.out.println("|\\_/|");
+    System.out.println("|q p|   /}");
+    System.out.println("( 0 )"+ quotes + quotes + quotes + "\\");
+    System.out.println("|"+ quotes+ "^" + quotes+ "`    |");
+    System.out.println("||_/=\\\\__|");
+  }
+}
