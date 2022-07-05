30b3d95db14894594bb6585d019d746089b048a3
Yeji Koo <82144134+xlzbthxyjkoo@users.noreply.github.com>
2022-07-06 00:10:59 +0900

    Create 2741.java

---
+++ 3단계/2741.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        FastReader fr = new FastReader(); // 문제 해결 소스 코드
+
+        int n = fr.nextInt(); //테스트 개수
+
+        for(int i=0; i<n; i++) {
+          System.out.println(i+1);
+        }
+        
+
+    }
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+        public FastReader(String s) throws FileNotFoundException { br = new BufferedReader(new FileReader(new File(s))); }
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str;
+        }
+    }
+}
