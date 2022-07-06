85586da4df00ec066483187170a5b8a03ca1c737
Yeji Koo <82144134+xlzbthxyjkoo@users.noreply.github.com>
2022-07-06 15:00:47 +0900

    Create 1110.java

---
+++ 3단계/1110.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        FastReader fr = new FastReader(); // 문제 해결 소스 코드
+        int n = fr.nextInt();
+        int temp = n;  //초기값 저장
+        int count = 0; //count
+
+        while(true) {
+          
+          n = ((n%10)*10) + (((n/10)+(n%10))%10);
+          count++;
+      
+          if (temp == n) {
+            break;
+          }
+
+        }
+        System.out.println(count);
+
+    }
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+        public FastReader(String s) throws FileNotFoundException { br = new BufferedReader(new FileReader(new File(s))); }
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str;
+        }
+    }
+}
