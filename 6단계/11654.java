78254c54dea09664822e2a3bde55abf577fa2eb3
Yeji Koo <82144134+xlzbthxyjkoo@users.noreply.github.com>
2022-07-08 23:06:00 +0900

    Create 11654.java

---
+++ 6단계/11654.java
@@ -0,0 +1,64 @@
+
+
+import java.util.*;
+import java.io.*;
+    
+public class Main {
+  
+  public static void main(String[] args) throws IOException {
+    FastReader fr = new FastReader();
+
+    char ch = fr.nextLine().charAt(0); 
+    int ascii = ch;  //가져온 char을 int 형으로 변환
+
+
+    System.out.println(ascii);
+
+  }
+
+  public static class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public FastReader(String s) throws FileNotFoundException {
+      br = new BufferedReader(new FileReader(new File(s)));
+    }
+
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+}
