dc69887dce57d07f7569e338b83acf9ed1c491c9
Yeji Koo <82144134+xlzbthxyjkoo@users.noreply.github.com>
2022-07-30 16:29:56 +0900

    Create 2292.java

---
+++ 7단계/2292.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        FastReader fr = new FastReader(); // 문제 해결 소스 코드
+        int N = fr.nextInt(); //주어진 방의 개수
+        
+        int num = 1; //1부터 진행 
+        int count = 1;
+        
+        while(num < N) {
+        	num += 6*count;  //6의 배수만큼 더해가기
+        	count++;
+        }
+        
+        System.out.println(count);
+
+    }
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+        public FastReader(String s) throws FileNotFoundException { br = new BufferedReader(new FileReader(new File(s))); }
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str;
+        }
+    }
+}
