29a6edf344eb000263d7356446b2b27dbbcf6da7
Yeji Koo <82144134+xlzbthxyjkoo@users.noreply.github.com>
2022-08-31 00:08:46 +0900

    Create 1427.java

---
+++ 11단계/1427.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    FastReader fr = new FastReader(); // 문제 해결 소스 코드
+ 
+		char[] ary = fr.nextLine().toCharArray();
+
+		Arrays.sort(ary);  //오름차순으로 정렬
+ 
+		for (int i = ary.length - 1; i >= 0; i--) {  //반대로 출력 -> 내림차순
+			System.out.print(ary[i]);
+		}
+    
+  }
+
+  public static class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public FastReader(String s) throws FileNotFoundException {
+      br = new BufferedReader(new FileReader(new File(s)));
+    }
+
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+}
