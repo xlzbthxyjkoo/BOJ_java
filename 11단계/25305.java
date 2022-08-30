32f0dd141b84d2fe76dfc7c577148078fe7050e9
Yeji Koo <82144134+xlzbthxyjkoo@users.noreply.github.com>
2022-08-30 23:23:54 +0900

    Create 25305.java

---
+++ 11단계/25305.java
@@ -0,0 +1,69 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    FastReader fr = new FastReader(); // 문제 해결 소스 코드
+    int N = fr.nextInt();
+    
+    int k = fr.nextInt();
+
+    Integer ary[] = new Integer[N];
+
+    for(int i = 0; i < N; i++) {
+      ary[i] = fr.nextInt();
+    }
+
+    Arrays.sort(ary, Comparator.reverseOrder());
+    
+    System.out.println(ary[k-1]);
+
+    
+  }
+
+  public static class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public FastReader(String s) throws FileNotFoundException {
+      br = new BufferedReader(new FileReader(new File(s)));
+    }
+
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+}
