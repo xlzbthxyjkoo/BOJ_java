3331f8351cf9fea34ae5672453b90d02d2465404
Yeji Koo <82144134+xlzbthxyjkoo@users.noreply.github.com>
2023-01-30 23:17:36 +0800

    Update 1157.java

--- 6단계/1157.java
+++ 6단계/1157.java
@@ -3,7 +3,7 @@
 import java.util.*;
 import java.io.*;
     
-public class Main {
+public class Main{
   
   public static void main(String[] args) throws IOException {
     FastReader fr = new FastReader();
