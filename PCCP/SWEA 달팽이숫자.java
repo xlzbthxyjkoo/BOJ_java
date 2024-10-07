016dbd2eab6b6df3dfb10c12e08a7debc4598d0d
Yeji Koo <82144134+xlzbthxyjkoo@users.noreply.github.com>
2024-10-08 00:01:57 +0900

    Create SWEA 달팽이숫자.java

---
+++ PCCP/SWEA 달팽이숫자.java
@@ -0,0 +1,94 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+        static int [] dr = {0, 1, 0, -1};
+        static int [] dc = {1, 0, -1, 0};
+	public static void main(String args[]) throws Exception
+	{
+		/*
+		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
+		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
+		 */
+		//System.setIn(new FileInputStream("res/input.txt"));
+
+		/*
+		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
+		 */
+                  
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			//N입력 받기
+            int N = sc.nextInt();
+            //우하좌상 ++하면서 돌아야 함
+            int [][] arr = new int[N][N];
+            int r = 0, c = 0, d = 0;
+            for(int i = 1; i <= N*N; i++){
+                //첫번째 좌표에 1넣고 시작
+                arr[r][c] = i;
+                //좌표 계산만 해보기
+                int nr = r + dr[d];
+                int nc = c + dc[d];
+                //범위에서 벗어나거나 값이 이미 있는지 확인
+                if(nr < 0 || nr >= N || nc < 0 || nc >= N || arr[nr][nc] != 0){
+                    //방향 바꿔주기
+                    d = (d + 1) % 4;
+                    //좌표 다시 정의해주기
+                    nr = r + dr[d];
+                    nc = c + dc[d];
+                }
+                r = nr;
+                c = nc;  
+            }
+            System.out.println("#" + test_case);
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j < N; j++){
+                    System.out.print(arr[i][j] + " ");
+                }
+                System.out.println();          
+            }
+		}
+	}
+}
